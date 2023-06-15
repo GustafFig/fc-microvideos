1ac357b25818973bd07b80b6a0a059dd225de717
Gustavo Figueiredo <gustavohmfigueiredo@gmail.com>
2023-06-15 18:47:32 +0000

    test: add test e2e of post

--- src/django_app/category/tests/e2e/categories/test_e2e_post.py
+++ src/django_app/category/tests/e2e/categories/test_e2e_post.py
@@ -1,4 +1,6 @@
 import pytest
+from core.__seedwork.infra.testing_helpers import make_request
+from django_app.category.tests.api.helpers import mock_category_serializer_validators
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.test import APIClient
@@ -29,6 +31,20 @@ class TestCategoriesPostE2E:
         response = self.make_requisition(http_expect.request.body)
         assert response.status_code == 422
         assert response.content == JSONRenderer().render(http_expect.exception.detail)
+
+    @pytest.mark.parametrize(
+        'http_expect', CreateCategoryApiFixture.arrange_for_entity_validation_error()
+    )
+    def test_entity_validation_error(self, http_expect: HttpExpect):
+        """
+        Remove the http validation, that is previous domain validation
+        """
+        with mock_category_serializer_validators(http_expect) as mockSerializer:
+            response = self.make_requisition(http_expect.request.body)
+        mockSerializer.mock_is_valid.assert_called()
+        mockSerializer.mock_validated_data.assert_called()
+        assert response.status_code == 422
+        assert response.content == JSONRenderer().render(http_expect.exception.error)
 
     @pytest.mark.parametrize('http_expect', CreateCategoryApiFixture.arrange_for_save())
     def test_post(self, http_expect: HttpExpect):
--- src/django_app/exception_handler.py
+++ src/django_app/exception_handler.py
@@ -1,4 +1,6 @@
+from core.__seedwork.domain.exceptions import ValidationException
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from rest_framework.views import exception_handler as rest_framework_exception_handler
 
@@ -8,7 +10,19 @@ def handler_serializer_validation_error(exception: ValidationError, context):
     response.status_code = 422
     return response
 
+
+def handler_serializer_validation_exception(
+    exception: ValidationException, context,   # pylint: disable=unused-argument
+):
+    response = Response(exception.error, status=422)
+    return response
+
+handlers = {
+    ValidationError: handler_serializer_validation_error,
+    ValidationException: handler_serializer_validation_exception,
+}
+
+
 def custom_exception_handler(exc, context):
-    if isinstance(exc, ValidationError):
-        return handler_serializer_validation_error(exc, context)
-    return rest_framework_exception_handler(exc, context)
+    handler = handlers.get(exc.__class__, rest_framework_exception_handler)
+    return handler(exc, context)
